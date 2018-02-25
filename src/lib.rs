2abd0c8a06fab78a2759292860e0b708af719e7b
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-02-25 12:33:41 +0100

    Update lib for egl

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,2 @@
-/* Explanation
- * You don't need no_mangle if the function is not exported directly
- * Is extern mandatory ? No because it affects symbol but WE are using the address
- * is pub mandatory ? Yes if it's an inner module
-*/
-#[cfg(feature = "foo")]
-pub mod modu;
-
-
-#[no_mangle]
-pub extern "C" fn getProcAddr() -> *mut() {
-    modu::toto();
-    modu::hello as *mut()
-}
-
+#[cfg(feature = "egl")]
+pub mod egl;
