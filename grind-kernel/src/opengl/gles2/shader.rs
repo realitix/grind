0eb3215a81328aa94789305a4c98588d31070052
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-05-18 15:53:37 +0200

    Implement ShaderString

--- grind-kernel/src/opengl/gles2/shader.rs
+++ grind-kernel/src/opengl/gles2/shader.rs
@@ -1,20 +1,34 @@
+use std::ffi::CStr;
+use std::mem;
+
 use opengl::types::*;
 
 
 pub struct Shader {
     pub id: GLuint,
-    shader_type: GLenum
+    shader_type: GLenum,
+    source: String
 }
 
 impl Shader {
     pub fn new(id: GLuint, shader_type: GLenum) -> Shader {
         Shader {
             id,
-            shader_type
+            shader_type,
+            source: String::new()
         }
     }
 
     pub fn set_source(&mut self, count: GLsizei, string: *const *const GLchar, length: *const GLint) {
+        let mut result = String::new();
+        for i in 0..count {
+            let slice = unsafe { CStr::from_ptr(*string.offset(i as isize)) };
+            let clean_slice = slice.to_str().unwrap();
+            result.push_str(clean_slice);
+            result.push('\n');
+        }
+
+        mem::replace(&mut self.source, result);
     }
 }
 
